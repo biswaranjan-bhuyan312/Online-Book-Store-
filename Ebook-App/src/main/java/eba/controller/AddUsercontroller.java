440e3fd3506e080b559ee5e8c1a5b45cab029c18
soumy <soumy@Soumya>
2024-10-07 13:33:26 +0530

    controler

--- Ebook-App/src/main/java/eba/controller/AddUsercontroller.java
+++ Ebook-App/src/main/java/eba/controller/AddUsercontroller.java
@@ -48,7 +48,7 @@ public class AddUsercontroller extends HttpServlet {
 				
 				UserDao userDao =new UserDao();
 				if(userDao.addUser(user)) {
-					request.setAttribute("Succmsg", "Registration Successful");
+					request.setAttribute("Succmsg", "Registration Successfuly");
 				}else {
 					request.setAttribute("failedmsg", "There is some issues");
 				}
